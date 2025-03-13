484d9d2d7a0e37c49871c1a52987df207c6fb359
SCSC <scsc@SCSC>
2025-03-13 21:21:20 +0900

    eslint 에러 수정

--- src/app/edit-post/[postId]/page.tsx
+++ src/app/edit-post/[postId]/page.tsx
@@ -1,10 +1,6 @@
 // app/edit-post/[postId]/page.tsx (서버 컴포넌트)
 import PostEditor from "@/components/post-editor";
 
-export default function EditPostPageWrapper({
-  params,
-}: {
-  params: { postId: string };
-}) {
+export default function EditPostPageWrapper({ params }: { params: any }) {
   return <PostEditor postId={params.postId} />;
 }
